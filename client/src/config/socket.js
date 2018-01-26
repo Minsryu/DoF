3abd7fbcf0220fcb4300dfa018330b1941c7422d
Min <myyms@Mins-MacBook-Pro.local>
2018-01-25 16:32:36 -0800

    testierialwher

--- client/src/config/socket.js
+++ client/src/config/socket.js
@@ -3,7 +3,7 @@ var io = require("socket.io-client");
 class Socket {
 	constructor() {
 		// connect to server
-		this.socket = (window.location.href.indexOf("heroku")!==-1) ? window.io() : io.connect("http://localhost:3001");
+		this.socket = (window.location.href.indexOf("heroku")!==-1) ? io(window.location.origin); : io.connect("http://localhost:3001");
 	}
 }
 
